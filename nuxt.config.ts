93714676e320fb71a4014ae2817cb9a146e9f169
annts095 <anantasuku@team-lab.com>
2023-12-06 13:55:26 +0900

    fix: デフォルトのレンダリング方式をCSRにする

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // デプロイ設定
+  // 1. CSR
+    devtools: { enabled: true },
+    ssr: false,
+
+  // 2. Serverless on lambda
+  /*
   nitro: {
     preset: 'aws-lambda',
     serveStatic: true,
   },
+  */
 })
